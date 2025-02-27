3766ee8288bcb8aa0b7766a6d4bf6b957831e20c
TON14163 <78469565+TON14163@users.noreply.github.com>
2025-02-27 15:47:27 +0700

    login

--- config/database.php
+++ config/database.php
@@ -4,4 +4,10 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 mysqli_set_charset($conn,"utf8");
+
+$new = mysqli_connect("localhost","testt","testt","testt");
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+mysqli_set_charset($new,"utf8");
 ?>
