3eb1f11d89ec7d22b945ffe567c63d3478a9df58
TON14163 <78469565+TON14163@users.noreply.github.com>
2025-02-27 15:39:29 +0700

    login page

--- config/database.php
+++ config/database.php
@@ -1,1 +1,7 @@
-db
+<?php
+$conn = mysqli_connect("localhost","test","test","test");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+mysqli_set_charset($conn,"utf8");
+?>
