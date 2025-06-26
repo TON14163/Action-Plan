1c21957868f3ada60e9652520cf8fe56ec71e2a0
TON14163 <78469565+TON14163@users.noreply.github.com>
2025-06-26 09:15:42 +0700

    รายงานสรุปเสนอราคา แก้ไขการค้นหา ชื่อสินค้า ไม่ขึ้น

--- src/views/report_quotation.php
+++ src/views/report_quotation.php
@@ -398,5 +398,30 @@ require_once __DIR__ . '/layouts/Main.php';
             dropdown.style.display = 'none';
         }
     });
+
+
+
+
+// ชื่อสินค้า START
+    function addProductRow(rowNum, fieldName, searchTerm, txtHint, product_twolist) {
+        if (!searchTerm.trim() || searchTerm.length == 0) {
+            document.getElementById(`${txtHint}`).innerHTML = "";
+            document.getElementById(`${txtHint}`).style.display = "none";
+            return;
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                document.getElementById(`${txtHint}`).innerHTML = this.responseText;
+                document.getElementById(`${txtHint}`).style.display = "block";
+            }
+        };
+        
+        xhr.open("GET", "./src/controllers/product_list_controllers.php?q=" + encodeURIComponent(searchTerm) + "&rowNum=" + rowNum + "&fieldName=" + encodeURIComponent(fieldName) + "&txtHint=" + encodeURIComponent(txtHint) + "&product_twolist=" + encodeURIComponent(product_twolist), true);
+        xhr.send();
+    }
+// ชื่อสินค้า END
 </script>
-<script src="<?php echo $_SESSION['thisDomain'];?>/assets/js/addProductRow.js"></script> <!-- ชื่อสินค้า -->
+
+<!-- <script src="<?php // echo $_SESSION['thisDomain'];?>assets/js/addProductRow.js"></script> ชื่อสินค้า -->
